1509f28d30a613c2c5eb90ebcf5830faf50b796a
Hossam-Raafat <juggiiboy@gmail.com>
2017-05-21 18:58:03 +0200

    Add routes for question CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   namespace 'manager' do
     mount_devise_token_auth_for 'Manager', at: 'auth'
   end
+
+  resources :questions
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
