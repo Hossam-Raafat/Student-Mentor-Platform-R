17e67fd14527733f60e633ad4d014754fbf54e9a
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2017-06-02 11:50:03 +0200

    add url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://mentor-scheduling.herokuapp.com/cable" 
+  config.action_cable.url = "ws://mentor-scheduling.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://mentor-scheduling.netlify.com']
 end
