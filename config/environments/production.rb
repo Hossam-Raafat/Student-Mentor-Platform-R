c34f0797696c53d59b0a3163604360f15e6eb87f
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2017-06-02 13:16:32 +0200

    fix prod sendgrid conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,15 +49,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "backend_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
